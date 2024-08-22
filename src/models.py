960875888f6603329cb57059e90610924cbd1224
Maryanni <52219648+Maryanni@users.noreply.github.com>
2024-08-22 20:33:24 +0000

    Solution

--- src/models.py
+++ src/models.py
@@ -7,26 +7,152 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email =Column(String(250), nullable=False)
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    year = Column(String(250), nullable=False)
+    driver = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite_vehicle(Base):
+    __tablename__ = 'favorite_vehicle'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    vehicle = relationship(Vehicle)
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+
+
+class Favorite_character(Base):
+    __tablename__ = 'favorite_character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    lenght = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+
+
+class Favorite_planet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+class Film(Base):
+    __tablename__ = 'film'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    director = Column(String(250), nullable=False)
+    producer = Column(String(250), nullable=False)
+    episode = Column(String(250), nullable=False)
+    opening_crawl = Column(String(250), nullable=False)
+
+
+class Favorite_film(Base):
+    __tablename__ = 'favorite_film'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    film_id = Column(Integer, ForeignKey('film.id'))
+    film = relationship(Film)
+
+
+class Specie(Base):
+    __tablename__ = 'specie'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    classification = Column(String(250), nullable=False)
+    designation = Column(String(250), nullable=False)
+    average_height = Column(String(250), nullable=False)
+    average_lifespan = Column(String(250), nullable=False)
+    hair_colors = Column(String(250), nullable=False)
+    skin_colors = Column(Integer, nullable=False)
+    eye_colors = Column(String(250), nullable=False)
+    language = Column(String(250), nullable=False)
+
+
+class Favorite_specie(Base):
+    __tablename__ = 'favorite_specie'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    specie_id = Column(Integer, ForeignKey('specie.id'))
+    specie = relationship(Specie)
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    length = Column(Integer, nullable=False)
+    passengers = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+
+
+class Favorite_starship(Base):
+    __tablename__ = 'favorite_starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    starship_id = Column(Integer, ForeignKey('starship.id'))
+    starship = relationship(Starship)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
